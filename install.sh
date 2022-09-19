6a68734a87a50bd74ce60e8f231ce9256e9b0535
Aaron Elliott <mraarone@yahoo.com>
2022-09-19 14:22:45 -0500

    Added zsh plugins.

--- install.sh
+++ install.sh
@@ -9,6 +9,12 @@ DOTFILES_LOCATION=$(pwd)
 export DOTFILES_LOCATION;
 
 ###
+# Change time zone
+###
+sudo ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+###
 # Install dependencies
 ###
 #./bin/dotfiles install starship
@@ -23,3 +29,11 @@ export DOTFILES_LOCATION;
 #./bin/dotfiles install mongodb
 #./bin/dotfiles install tmux
 #./bin/dotfiles install vim
+
+zshrc() {
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+}
+
+zshrc
