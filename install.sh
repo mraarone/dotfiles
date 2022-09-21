cefbca49796d336d79ca0f88f6382c60f042429e
mraarone <mraarone@yahoo.com>
2022-09-20 23:12:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,9 +17,17 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 ###
 # Install dependencies
 ###
+zshrc() {
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+}
+
+zshrc
+
 #./bin/dotfiles install starship
-./bin/dotfiles install omz
 ./bin/dotfiles install zsh
+./bin/dotfiles install omz
 ./bin/dotfiles install powerlevel10k
 ./bin/dotfiles install vscode
 ./bin/dotfiles install dircolors
@@ -30,11 +38,3 @@ sudo dpkg-reconfigure --frontend noninteractive tzdata
 #./bin/dotfiles install mongodb
 #./bin/dotfiles install tmux
 #./bin/dotfiles install vim
-
-zshrc() {
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-}
-
-zshrc
