691c98ee60811c7ce1a23abd586f0008c7843a68
mraarone <mraarone@yahoo.com>
2022-09-24 01:05:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,17 +21,21 @@ sudo /usr/sbin/dpkg-reconfigure --frontend noninteractive tzdata
 ###
 # Install dependencies
 ###
-zshrc() {
+omz_plugins() {
+    if [ -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then rm -rf ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions; fi
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+    if [ -d ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; then rm -rf ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]; fi
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+    if [ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]; then rm -rf ~/.oh-my-zsh/custom/themes/powerlevel10k; fi
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 }
 
-zshrc
-
 #./bin/dotfiles install starship
-./bin/dotfiles install zsh
 ./bin/dotfiles install omz
+omz_plugins
+./bin/dotfiles install zsh
 ./bin/dotfiles install powerlevel10k
 ./bin/dotfiles install vscode
 ./bin/dotfiles install dircolors
