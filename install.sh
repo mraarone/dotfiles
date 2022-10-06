d3b049eca1e8fd501a12de15fecc644c9b5cacd6
mraarone <mraarone@yahoo.com>
2022-10-06 12:10:27 -0500

    Added automated /tmp/dotfilesLog.txt logging

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/tmp/dotfilesLog.txt 2>&1
+# Everything below will go to the file 'dotfilesLog.txt':
+
 ###
 # Installation of packages, configurations, and dotfiles.
 ###
